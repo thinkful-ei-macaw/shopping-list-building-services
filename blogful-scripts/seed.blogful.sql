c9a2abca88c2498d087058f935bcc8c62958255a
dasha akhten <dasha.akhten@gmail.com>
2020-04-02 14:13:30 -0700

    first commit

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+INSERT INTO blogful_articles (title, content, date_published)
+
+VALUES
+('OKuuurrr', 'Dogs are cool! Many console options'),
+('Yaaaaas', 'Cats are cooler! Many console options'),
+('Teach me how to dougie’, ‘Fishes are coolest! Many console options’),
+(‘I need to do the millie wap’, ‘Zebras have stripes! Many console options’),
+(’I’‘m not a rapper’, ‘Snakes have no legs! Many console options’),
+(‘But I am’, ‘Monkeys are humans! Many console options’),
+(‘How to make a viral dance on Tikok’, ‘Ants are small’),
+(‘Do the whobble’, ‘Again, dogs are cool! Many console options’),
+(‘Im just saying you can do better’, ‘Parrots are smart! Many console options’),
+(‘Kiki do you love me?’, ‘Hamsters are boring! Many console options’),
+(‘Are you riding?’, ‘Mountains are big! Many console options’),
+(’Because I’‘m down for you always’, ‘Sky is blue! Many console options’),
+(‘Drake is life’, ‘Grass is green! Many console options’),
+(‘I live for Beyonce’, ‘Rocks are hard! Many console options’),
+(‘Beyonce knows I love her’, ‘People are nice! Many console options’),
+(‘I love music’, ‘Thinkful is tight! Many console options’),
+(‘NOW 5’, ‘I am a coder! Many console options’),
+(‘NOW 6’, ‘You are a coder! Many console options’),
+(‘NOW 7’, ‘We are coders! Many console options’),
+(‘NOW 8’, ‘Coding is life! Many console options’)
+;
